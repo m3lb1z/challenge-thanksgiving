3676fbd72aac3b3c71fd11c5a423655c74c67b80
Elvis M. <u201112959@upc.edu.pe>
2024-11-21 11:24:12 -0500

    build: add maven wrapper and initial project structure

---
+++ src/main/java/dev/emrx/thanksgiving/ThanksgivingApplication.java
@@ -0,0 +1,13 @@
+package dev.emrx.thanksgiving;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThanksgivingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThanksgivingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/emrx/thanksgiving/ThanksgivingApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.emrx.thanksgiving;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThanksgivingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
